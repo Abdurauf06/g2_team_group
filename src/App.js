dfc03bfe3822592fef5820425a9cb5c8dfa22529
abdurauf20o6 <yusufjanovhasanboy22@gmail.com>
2023-05-21 22:21:36 +0500

    hgf6+

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-    do'stlar
-    hasanboy
-    hammaga salom
-    salom
-    salom2
-    6ced6767370d3acb7412ded12e5a38523d92b470
-
-    maxadan salom
-    5b824684d1ad45dcf26570bfb12a545feadb1bd9
+      
     </div>
   );
 }
